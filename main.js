9537e18945cd2da40dce8da459216399065a1fba
ruslana_hural <rusjah1@gmail.com>
2023-11-12 00:23:39 +0100

    styled page

---
+++ main.js
@@ -0,0 +1,9 @@
+const products = [
+    {
+        id: Math.floor(Math.random() * 100000),
+        name: 'Donner DDP-80 Digital-Piano',
+        price: 679.99,
+        img: '"https://images.unsplash.com/photo-1552422535-c45813c61732?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGlhbm98ZW58MHx8MHx8fDA%3D'
+    }
+]
+
